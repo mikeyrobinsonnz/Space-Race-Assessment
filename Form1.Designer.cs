fe93584537620e49d578008039fd531aa52f48f8
Mikey Robinson <mikeyrobinson@mmc.school.nz>
2020-08-18 14:29:37 +1200

    Main Panel Completed

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Space_Race
+{
+    partial class FrmSpace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSpace));
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.PictureMain = new System.Windows.Forms.PictureBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.PnlGame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.BackColor = System.Drawing.SystemColors.Desktop;
+            this.PnlGame.Controls.Add(this.BtnStart);
+            this.PnlGame.Controls.Add(this.PictureMain);
+            this.PnlGame.Location = new System.Drawing.Point(12, 12);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(750, 400);
+            this.PnlGame.TabIndex = 0;
+            this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
+            // 
+            // PictureMain
+            // 
+            this.PictureMain.Image = ((System.Drawing.Image)(resources.GetObject("PictureMain.Image")));
+            this.PictureMain.Location = new System.Drawing.Point(3, 3);
+            this.PictureMain.Name = "PictureMain";
+            this.PictureMain.Size = new System.Drawing.Size(750, 400);
+            this.PictureMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureMain.TabIndex = 0;
+            this.PictureMain.TabStop = false;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.Color.White;
+            this.BtnStart.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStart.Location = new System.Drawing.Point(300, 300);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(164, 76);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = false;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // FrmSpace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PnlGame);
+            this.Name = "FrmSpace";
+            this.Text = "Form1";
+            this.PnlGame.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.PictureBox PictureMain;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Race
+{
+    public partial class FrmSpace : Form
+    {
+        public FrmSpace()
+        {
+            InitializeComponent();
+            PictureMain.Visible = true;
+            BtnStart.Visible = true;
+
+        }
+
+        private void PnlGame_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            PictureMain.Visible = false;
+            BtnStart.Visible = false;
+         
+               
+        }
+    }
+}
