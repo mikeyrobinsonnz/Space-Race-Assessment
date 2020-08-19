1a6a6943df1b3d8a1aff425333d82fdd66720d65
Mikey Robinson <mikeyrobinson@mmc.school.nz>
2020-08-20 10:55:50 +1200

    all panel work done, ui completed.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,8 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSpace));
             this.PnlGame = new System.Windows.Forms.Panel();
-            this.PictureMain = new System.Windows.Forms.PictureBox();
+            this.lblinstruct1 = new System.Windows.Forms.Label();
             this.BtnStart = new System.Windows.Forms.Button();
+            this.PictureMain = new System.Windows.Forms.PictureBox();
+            this.lblinstruct2 = new System.Windows.Forms.Label();
+            this.btncontinue = new System.Windows.Forms.Button();
             this.PnlGame.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PictureMain)).BeginInit();
             this.SuspendLayout();
@@ -39,6 +42,9 @@
             // PnlGame
             // 
             this.PnlGame.BackColor = System.Drawing.SystemColors.Desktop;
+            this.PnlGame.Controls.Add(this.btncontinue);
+            this.PnlGame.Controls.Add(this.lblinstruct2);
+            this.PnlGame.Controls.Add(this.lblinstruct1);
             this.PnlGame.Controls.Add(this.BtnStart);
             this.PnlGame.Controls.Add(this.PictureMain);
             this.PnlGame.Location = new System.Drawing.Point(12, 12);
@@ -47,15 +53,18 @@
             this.PnlGame.TabIndex = 0;
             this.PnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.PnlGame_Paint);
             // 
-            // PictureMain
+            // lblinstruct1
             // 
-            this.PictureMain.Image = ((System.Drawing.Image)(resources.GetObject("PictureMain.Image")));
-            this.PictureMain.Location = new System.Drawing.Point(3, 3);
-            this.PictureMain.Name = "PictureMain";
-            this.PictureMain.Size = new System.Drawing.Size(750, 400);
-            this.PictureMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.PictureMain.TabIndex = 0;
-            this.PictureMain.TabStop = false;
+            this.lblinstruct1.AutoSize = true;
+            this.lblinstruct1.BackColor = System.Drawing.Color.White;
+            this.lblinstruct1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblinstruct1.ForeColor = System.Drawing.Color.Black;
+            this.lblinstruct1.Location = new System.Drawing.Point(122, 170);
+            this.lblinstruct1.Name = "lblinstruct1";
+            this.lblinstruct1.Size = new System.Drawing.Size(522, 21);
+            this.lblinstruct1.TabIndex = 2;
+            this.lblinstruct1.Text = "Instructions: Use The Arrow Keys to Guide your Spaceship";
+            this.lblinstruct1.Click += new System.EventHandler(this.label1_Click);
             // 
             // BtnStart
             // 
@@ -69,6 +78,39 @@
             this.BtnStart.UseVisualStyleBackColor = false;
             this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
+            // PictureMain
+            // 
+            this.PictureMain.Image = ((System.Drawing.Image)(resources.GetObject("PictureMain.Image")));
+            this.PictureMain.Location = new System.Drawing.Point(3, 0);
+            this.PictureMain.Name = "PictureMain";
+            this.PictureMain.Size = new System.Drawing.Size(750, 400);
+            this.PictureMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureMain.TabIndex = 0;
+            this.PictureMain.TabStop = false;
+            // 
+            // lblinstruct2
+            // 
+            this.lblinstruct2.AutoSize = true;
+            this.lblinstruct2.BackColor = System.Drawing.Color.White;
+            this.lblinstruct2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblinstruct2.Location = new System.Drawing.Point(70, 191);
+            this.lblinstruct2.Name = "lblinstruct2";
+            this.lblinstruct2.Size = new System.Drawing.Size(634, 21);
+            this.lblinstruct2.TabIndex = 3;
+            this.lblinstruct2.Text = "Through the moving obstacles, reach the top of the panel to get a point.";
+            // 
+            // btncontinue
+            // 
+            this.btncontinue.Font = new System.Drawing.Font("Myriad Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncontinue.ForeColor = System.Drawing.Color.Red;
+            this.btncontinue.Location = new System.Drawing.Point(297, 243);
+            this.btncontinue.Name = "btncontinue";
+            this.btncontinue.Size = new System.Drawing.Size(167, 41);
+            this.btncontinue.TabIndex = 4;
+            this.btncontinue.Text = "Continue";
+            this.btncontinue.UseVisualStyleBackColor = true;
+            this.btncontinue.Click += new System.EventHandler(this.btncontinue_Click);
+            // 
             // FrmSpace
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -76,8 +118,9 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.PnlGame);
             this.Name = "FrmSpace";
-            this.Text = "Form1";
+            this.Text = "Space Race";
             this.PnlGame.ResumeLayout(false);
+            this.PnlGame.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PictureMain)).EndInit();
             this.ResumeLayout(false);
 
@@ -88,6 +131,9 @@
         private System.Windows.Forms.Panel PnlGame;
         private System.Windows.Forms.Button BtnStart;
         private System.Windows.Forms.PictureBox PictureMain;
+        private System.Windows.Forms.Label lblinstruct1;
+        private System.Windows.Forms.Label lblinstruct2;
+        private System.Windows.Forms.Button btncontinue;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,10 @@ namespace Space_Race
             InitializeComponent();
             PictureMain.Visible = true;
             BtnStart.Visible = true;
+            lblinstruct1.Visible = false;
+            lblinstruct2.Visible = false;
+            PnlGame.Visible = true;
+            btncontinue.Visible = false;
 
         }
 
@@ -29,8 +33,25 @@ namespace Space_Race
         {
             PictureMain.Visible = false;
             BtnStart.Visible = false;
+            lblinstruct1.Visible = true;
+            lblinstruct2.Visible = true;
+            PnlGame.Visible = true;
+            btncontinue.Visible = true;
          
                
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncontinue_Click(object sender, EventArgs e)
+        {
+            lblinstruct1.Visible = false;
+            lblinstruct2.Visible = false;
+            PnlGame.Visible = true;
+            btncontinue.Visible = false;
         }
     }
 }
